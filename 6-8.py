da066a91122b473ef84ea727afecc5f5e5314b8e
Alex <alja.platt@gmail.com>
2022-07-06 19:45:22 +0100

    errors

--- 6-8.py
+++ 6-8.py
@@ -1,4 +1,4 @@
-import math
+import math, time
 #? Functions 
 
 # def performOperation(num1, num2, operation): 
@@ -226,3 +226,166 @@ uniqueList.append(1)
 uniqueList.append(2)
 
 print(uniqueList.someProperty)
+
+#* The super() function is used to give access to methods and properties of a parent or sibling class. The super() function returns an object that represents the parent class. 
+
+#* The self is used to represent the instance of the class. With this keyword, you can access the attributes and methods of the class in python. It binds the attributes with the given arguments.  
+
+#! 8. ERRORS
+
+# def causeError():
+#     return 1/0
+
+# causeError() #? ZeroDivisionError: division by zero
+
+# def causeError():
+#     return 1/0
+
+# def callCauseError():
+#     return causeError()
+
+# callCauseError() 
+
+#* TRY / EXCEPT
+#? Catch exception so it doesn't cause an err  
+
+# try: 
+#     1/0
+# except Exception as e: 
+#     print(type(e)) #todo - <class 'ZeroDivisionError'>
+
+# def causeError():
+#     try: 
+#         return 1/0
+#     except Exception as e: 
+#         return e #todo - <class 'ZeroDivisionError'>
+
+# causeError()
+
+#? HANDLING EXCEPTIONS 
+
+# def causeError():
+#     try:
+#         return 1/0
+#     except Exception:
+#         print('There was some sort of error!')
+    
+# causeError()
+
+#* FINALLY
+
+# def causeError():
+#     try:
+#         return 1/1
+#     except Exception:
+#         print('There was some sort of error!')
+#     finally:
+#         print('This will always execute!')
+    
+# causeError()
+
+# def causeError():
+#     start = time.time()
+#     try:
+#         time.sleep(0.5)
+#         return 1/0
+#     except Exception:
+#         print('There was some sort of error!')
+#     finally:
+#         print(f'Function took {time.time() - start} seconds to execute')
+    
+# causeError()
+
+#* CATCHING ERR BY TYPE
+
+def causeError():
+    try:
+        return 1 + 'a'
+
+    except TypeError:
+        print('There was a type error!')
+    except ZeroDivisionError:
+        print('There was a zero division error!')
+    except Exception:
+        print('There was some sort of error!')
+
+    
+causeError()
+
+#?  CUSTOM DECORATORS 
+
+def handleException(func):
+    def wrapper(*args):
+        try:
+            func(*args)
+        except TypeError:
+            print('There was a type error!')
+        except ZeroDivisionError:
+            print('There was a zero division error!')
+        except Exception:
+            print('There was some sort of error!')
+    return wrapper
+
+@handleException
+def causeError():
+    return 1/0
+
+causeError()
+
+#? RAISING EXCEPTIONS 
+
+@handleException
+def raiseError(n):
+    if n == 0:
+        raise Exception()
+    print(n)
+    
+raiseError(1)
+
+#! WRITING FUNCTIONS TO RAISE EXCEPTIONS IS POWERFUL WHEN YOU COMBINE IT WITH CUSTOM EXCEPTIONS
+
+#* CUSTOM EXCEPTIONS
+
+class CustomException(Exception):
+    pass
+
+
+def causeError():
+    raise CustomException('You called the causeError function!')
+    
+causeError()
+
+# CustomException                           Traceback (most recent call last)
+# Input In [3], in <module>
+#       5 def causeError():
+#       6     raise CustomException('You called the causeError function!')
+# ----> 8 causeError()
+
+# Input In [3], in causeError()
+#       5 def causeError():
+# ----> 6     raise CustomException('You called the causeError function!')
+
+# CustomException: You called the causeError function!
+
+#* ADDING ATTRIBUTES
+
+class HttpException(Exception):
+    statusCode = None
+    message = None
+    def __init__(self):
+        super().__init__(f'Status code: {self.statusCode} and message is: {self.message}')
+        
+class NotFound(HttpException):
+    statusCode = 404
+    message = 'Resource not found'
+    
+class ServerError(HttpException):
+    statusCode = 500
+    message = 'The server messed up!'
+    
+def raiseServerError():
+    raise ServerError()
+    
+raiseServerError()
+
+#* WRITING CUSTOM EXCEPTION MESSAGES IS A GREAT WAY TO KEEP CODE CLEAN AND ORGANISED. 
