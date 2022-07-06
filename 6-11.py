17ddfe454a72a713df6207379f15cedd6c1f7b36
Alex <alja.platt@gmail.com>
2022-07-06 13:46:45 +0100

    plotting ch

--- 6-11.py
+++ 6-11.py
@@ -68,19 +68,80 @@ print(localsFunction(1,2, operation='multiply'))
 
 print(globals())
 
-message = 'Some global data'
-varA = 2
+# message = 'Some global data'
+# varA = 2
+# def function1(varA, varB):
+#     message = 'Some local data'
+#     print(varA)
+#     print(message)
+#     print(locals())
+    
+    
+# def function2(varC, varB):
+#     print(varA)
+#     print(message)
+#     print(locals())
+    
+# function1(1, 2)
+# function2(3, 4)
+
 def function1(varA, varB):
     message = 'Some local data'
     print(varA)
-    print(message)
-    print(locals())
+    def inner_function(varA, varB):
+        print(f'inner_function local scope: {locals()}')
+        
+    inner_function(123, 456)
     
-    
-def function2(varC, varB):
-    print(varA)
-    print(message)
-    print(locals())
-    
-function1(1, 2)
-function2(3, 4)
+
+function1(1, 2) #? 1, inner_function local scope: {'varA': 123, 'varB': 456}
+
+#* FUNCTIONS AS VARIABLES
+
+text = '''
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+'''
+
+def lowercase(text):
+    return text.lower()
+
+def removePunctuation(text):
+    punctuations = ['.', '-', ',', '*']
+    for punctuation in punctuations:
+        text = text.replace(punctuation, '')
+    return text
+
+def removeNewlines(text):
+    text = text.replace('\n', ' ')
+    return text
+
+def removeShortWords(text):
+    return ' '.join([word for word in text.split() if len(word) > 3])
+
+def removeLongWords(text):
+    return ' '.join([word for word in text.split() if len(word) < 6])
+
+
+processingFunctions = [lowercase, removePunctuation, removeNewlines]
+
+for func in processingFunctions:
+    text = func(text)
+
+print(text)
+
+#* LAMBDA FUNCTION
+
+(lambda x: x + 3)(5)
+
+myList = [5,4,3,2]
+print(sorted(myList))
+
+myList = [{'num': 3}, {'num': 2}, {'num': 1}]
+sorted(myList, key=lambda x: x['num'])
+
+#? [{'num': 1}, {'num': 2}, {'num': 3}]
+
