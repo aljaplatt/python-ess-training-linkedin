a0f4b37f23e4c5aea93dee00fbf323f518e4b164
Alex <alja.platt@gmail.com>
2022-07-09 20:30:03 +0100

    count and lower

---
+++ 100-days/day-3/love-calc-37.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+namesLower  = name1.lower() + name2.lower()
+# name2Lower = name2.lower()
+t= namesLower.count('t')
+r= namesLower.count('r')
+u= namesLower.count('u')
+e= namesLower.count('e')
+l= namesLower.count('l')
+o= namesLower.count('o')
+v= namesLower.count('v')
+
+
+score = t + r + u + e + l + o + v
+
+
+
+
