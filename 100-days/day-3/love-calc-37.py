e754ac8ff890342dcbc2e0f09e70a980825f9cd9
Alex <alja.platt@gmail.com>
2022-07-10 06:55:41 +0100

    treasure-island

--- 100-days/day-3/love-calc-37.py
+++ 100-days/day-3/love-calc-37.py
@@ -11,12 +11,21 @@ t= namesLower.count('t')
 r= namesLower.count('r')
 u= namesLower.count('u')
 e= namesLower.count('e')
+true= t + r + u + e 
 l= namesLower.count('l')
 o= namesLower.count('o')
 v= namesLower.count('v')
+e= namesLower.count('e')
+love = l + o + v + e 
+love_score = int(str(true) + str(love))
 
+if love_score < 10 or love_score > 90:
+    print(f'Your love score is {love_score}, you go together like coke and mentos.')
+elif love_score >= 40 and love_score <= 50:
+    print(f'Your love score is {love_score}, you are alright together.')
+else:
+    print(f'Your score is {love_score}')
 
-score = t + r + u + e + l + o + v
 
 
 
