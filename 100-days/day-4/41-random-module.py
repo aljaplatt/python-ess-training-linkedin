a9e689b36bd5650fa901301e840746fe38896901
Alex <alja.platt@gmail.com>
2022-07-11 10:27:24 +0100

    random module

--- 100-days/day-4/41-random-module.py
+++ 100-days/day-4/41-random-module.py
@@ -5,3 +5,9 @@ random_int = random.randint(1,10)
 print(random_int)
 
 print(my_module.special_numbers)
+
+random_float = random.random() #* 0 -> 0.999999
+
+#* Random float between 0 & 5 ?
+
+random_float * 5 #* 0.0000... -> 4.99999....
---
+++ 100-days/day-4/42-heads-tails.py
@@ -0,0 +1,10 @@
+import random
+
+random_num = random.randint(1, 2)
+
+print(random_num)
+
+if random_num == 1:
+    print('Heads')
+else:
+    print('Tails')
