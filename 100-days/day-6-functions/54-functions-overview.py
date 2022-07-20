04fed477bb16fa21fce93cd3996d8cfddf57e266
Alex <alja.platt@gmail.com>
2022-07-20 07:52:45 +0100

    while loop

---
+++ 100-days/day-6-functions/54-functions-overview.py
@@ -0,0 +1,9 @@
+
+#? def = define
+
+def my_function():
+    print('Hi')
+    print('Bye')
+
+#* call 
+my_function()
---
+++ 100-days/day-6-functions/55-while-loop.py
@@ -0,0 +1,4 @@
+num_of_weeks_to_xmas = 10
+while num_of_weeks_to_xmas > 0:
+    num_of_weeks_to_xmas -= 1
+    print('Another week down')
