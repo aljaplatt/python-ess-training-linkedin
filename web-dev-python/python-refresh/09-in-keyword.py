df2523f020a06e636b0b13605603ae5dab5390b3
Alex <alja.platt@gmail.com>
2022-07-24 11:17:48 +0100

    in keyword

--- web-dev-python/python-refresh/09-in-keyword.py
+++ web-dev-python/python-refresh/09-in-keyword.py
@@ -9,6 +9,11 @@ print("Jen" in friends) #? True
 movies_watched = {"The Matrix", "Green Book", "Her"}
 user_movie = input("Enter something you've watched recently: ")
 
+if user_movie in movies_watched:
+    print(f"i've watched {user_movie}")
+else:
+    print('Not seen')
+    
 if user_movie not in movies_watched:
     movies_watched.add(user_movie)
 
