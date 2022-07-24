5d0f14967b288c299adf0b83d75d5fa1bedf8021
Alex <alja.platt@gmail.com>
2022-07-24 16:56:20 +0100

    cypher finished

--- 100-days/day-8_caeser.py/87-caeser-pt4.py
+++ 100-days/day-8_caeser.py/87-caeser-pt4.py
@@ -1,7 +1,3 @@
-from art import logo
-
-print(logo)
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
@@ -13,10 +9,12 @@ def caesar(start_text, shift_amount, cipher_direction):
     #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
     #e.g. start_text = "meet me at 3"
     #end_text = "•••• •• •• 3"
+    #* if character is in alphabet then do following
     if char in alphabet:
       position = alphabet.index(char)
       new_position = position + shift_amount
       end_text += alphabet[new_position]
+      #* else just add the character as it is... 
     else:
       end_text += char
   print(f"Here's the {cipher_direction}d result: {end_text}")
