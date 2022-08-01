eeda4636712802d0240e8da4d0a47f85e48fa38b
Alex <alja.platt@gmail.com>
2022-08-01 08:53:41 +0100

    recursion

--- 100-days/day-10_functions_with_outputs/103-calculator_pt1.py
+++ 100-days/day-10_functions_with_outputs/103-calculator_pt1.py
@@ -13,6 +13,7 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2 
 
+num1 = int(input('Pick number 1: '))
 
 operations = {
     '+': add,
@@ -21,11 +22,17 @@ operations = {
     '/': divide,
 }
 
-# num1 = int(input('Pick number 1: '))
-# num2 = int(input('Pick number 2: '))
+num2 = int(input('Pick number 2: '))
 
 #? will print out the key only  
 for symbol in operations:
     print(symbol)
 
 operation_symbol = input('Pick an operation from the line above: ')
+calculation_func = operations[operation_symbol]
+
+answer = calculation_func(num1, num2)
+
+
+
+print(f"{num1}{operation_symbol} {num2} = {answer}")
