2ab6e7aa9d1ef47d0b9afe68e549d2f5597e1732
Alex <alja.platt@gmail.com>
2022-08-10 20:22:59 +0100

    created game breakdown tasks

--- 100-days/day-14_high_low_game/game_sol.py
+++ 100-days/day-14_high_low_game/game_sol.py
@@ -1,10 +1,25 @@
+from art import logo
+from game_data import data
+import random 
+
+def format_data(acc):
+    #? format account data into printable format
+    account_name = acc['name']
+    account_desc = acc['description']
+    account_country = acc['country']
+    return f"{account_name}, a {account_desc}, from {account_country}"
+
 
 # ? Display art  
-
+print(logo)
 #* generate random account from the game data
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+    account_b = random.choice(data)
 
-#? format account data into printable format
-
+print(f"Compare A: {format_data(account_a)}")
+print(f"Compare B: {format_data(account_b)}")
 #* ask user for a guess A or B?
 
 #? Check if user is correct  
