fa120fe276806a1d7020cb31aa72f42b5a6adb9d
Alex <alja.platt@gmail.com>
2022-08-10 19:14:40 +0100

    created game breakdown tasks

---
+++ 100-days/day-14_high_low_game/game_sol.py
@@ -0,0 +1,22 @@
+
+# ? Display art  
+
+#* generate random account from the game data
+
+#? format account data into printable format
+
+#* ask user for a guess A or B?
+
+#? Check if user is correct  
+#* get follower count of each acc
+#? use if statement to check if user is correct   
+
+#* give user feedback on their guess  
+
+#? score keeping
+
+#* make game repeatable  
+
+#? making accounts at position b, move to position a 
+
+#* Clear terminal between rounds     
