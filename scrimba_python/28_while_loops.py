b82a8286906f04f483ea78773a0b52cac2ac7f4a
Alex <alja.platt@gmail.com>
2022-08-30 17:36:01 +0100

    28

--- scrimba_python/28_while_loops.py
+++ scrimba_python/28_while_loops.py
@@ -12,3 +12,16 @@ while count:
     stars += '*'
     print(f'{num}.{stars}Loops are great{stars}')
     count -= 1
+
+i=0
+while i < 5:
+    i+=1
+    print(f"{i}."+ "*"*i + "Loops are awesome" + "*"*i)
+    
+# Three Loop Questions:
+#1. What do I want to repeat?
+#  -> message
+#2. What do I want to change each time?
+#  -> stars
+#3. How long should we repeat?
+#  -> 5 times
