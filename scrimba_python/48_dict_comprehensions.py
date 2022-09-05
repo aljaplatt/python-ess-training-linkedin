78deffd036adce0a7ef887296b79d097462559e2
Alex <alja.platt@gmail.com>
2022-09-05 15:57:40 +0100

    dict comprehensions

---
+++ scrimba_python/48_dict_comprehensions.py
@@ -0,0 +1,5 @@
+# Dictionary comprehensions
+movies = ["And Now for Something Completely Different","Monty Python and the Holy Grail",
+"Monty Python's Life of Brian","Monty Python Live at the Hollywood Bowl","Monty Python's The Meaning of Life","Monty Python Live (Mostly)"]
+year =[1971,1975,1979,1982,1983,2014]
+names = ['John','Eric','Michael','Graham','Terry','TerryG']
